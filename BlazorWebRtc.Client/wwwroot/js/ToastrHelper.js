4d17d82071c131d62c8cf94eef23102ae75afd1f
alihangudenoglu <alihan_135@hotmail.com>
2024-11-07 15:40:30 +0300

    Added Toastr

---
+++ BlazorWebRtc.Client/wwwroot/js/ToastrHelper.js
@@ -0,0 +1,20 @@
+﻿
+toastr.options = {
+    "closeButton": true,
+    "progressBar": true,
+    "positionClass": "toast-bottom-right",
+    "timeOut": "3000"
+};
+// wwwroot/js/toastrHelper.js
+window.toastrNotify = (type, message, title) => {
+    console.log("trigger toastrNotify")
+    if (type === "success") {
+        toastr.success(message, title);
+    } else if (type === "error") {
+        toastr.error(message, title);
+    } else if (type === "info") {
+        toastr.info(message, title);
+    } else if (type === "warning") {
+        toastr.warning(message, title);
+    }
+};
